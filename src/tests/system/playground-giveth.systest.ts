5942b40013e14dea083543bd0bdfd5bf3f0521e0
iemwill <willie@laubenheimer.eu>
2019-10-30 17:19:00 +0100

    start to fix setup fund error

--- src/tests/system/playground-giveth.systest.ts
+++ src/tests/system/playground-giveth.systest.ts
@@ -4,6 +4,7 @@ import {
   Tracks,
   Deployment,
 } from '~/utils/environment/Environment';
+import { toBeTrueWith } from '../utils/toBeTrueWith';
 import {
   constructEnvironment,
   defaultOptions,
@@ -15,70 +16,81 @@ import { default as Web3Accounts } from 'web3-eth-accounts';
 //import { donateGivethETH } from '~/contracts/exchanges/transactions/donateGivethETH';
 //import { donateGivethERC20 } from '~/contracts/exchanges/transactions/donateGivethERC20';
 
-const { cliLogger } = require('~/utils/environment/cliLogger');
-const fs = require('fs');
+expect.extend({ toBeTrueWith });
 
-//Load deployment
-const _deployment: Deployment = JSON.parse(
-  fs.readFileSync(
-    'deployments/configs/ropsten-testing/001-givethModuleBeta-1.0.7.json',
-    'utf8',
-  ),
-);
+describe('playground', () => {
+  test('Happy path', async () => {
+    const { cliLogger } = require('~/utils/environment/cliLogger');
+    const fs = require('fs');
 
-//Create Web3 provider and account with private Key from keystore file.
-const _provider = new Web3Eth.providers.WebsocketProvider(
-  process.env.JSON_RPC_ENDPOINT,
-);
-const web3Accounts = new Web3Accounts(_provider);
-/*const account = web3Accounts.decrypt(
-	JSON.parse(fs.readFileSync(process.env.KEYSTORE_FILE, 'utf8')),
-	process.env.KEYSTORE_PASSWORD
-);*/
+    //Load deployment
+    const _deployment: Deployment = JSON.parse(
+      fs.readFileSync(
+        'deployments/configs/ropsten-testing/001-givethModuleBeta-1.0.7.json',
+        'utf8',
+      ),
+    );
 
-//Prepare wallet attributes
-const address = web3Accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
-const signTransaction = unsignedTransaction =>
-  web3Accounts
-    .signTransaction(unsignedTransaction, process.env.PRIVATE_KEY)
-    .then(t => t.rawTransaction);
-const signMessage = message =>
-  web3Accounts.sign(message, process.env.PRIVATE_KEY);
+    //Create Web3 provider and account with private Key from keystore file.
+    const _provider = new Web3Eth.providers.WebsocketProvider(
+      process.env.JSON_RPC_ENDPOINT,
+    );
+    const web3Accounts = new Web3Accounts(_provider);
+    const account = web3Accounts.decrypt(
+      JSON.parse(fs.readFileSync(process.env.KEYSTORE_FILE, 'utf8')),
+      process.env.KEYSTORE_PASSWORD,
+    );
 
-//Create wallet
-const _wallet = {
-  address,
-  signMessage,
-  signTransaction,
-};
+    //Prepare wallet attributes
+    const address = web3Accounts.privateKeyToAccount(account.privateKey);
+    const signTransaction = unsignedTransaction =>
+      web3Accounts
+        .signTransaction(unsignedTransaction, account.privateKey)
+        .then(t => t.rawTransaction);
+    const signMessage = message =>
+      web3Accounts.sign(message, account.privateKey);
 
-//Create Environment
-const environment: Environment = constructEnvironment({
-  endpoint: process.env.JSON_RPC_ENDPOINT,
-  provider: _provider,
-  logger: cliLogger,
-  deployment: _deployment,
-  options: defaultOptions,
-  wallet: _wallet,
-  track: Tracks.TESTING,
+    console.log('This here is a BUG: address is::', address);
+
+    //Create wallet
+    const _wallet = {
+      address,
+      signMessage,
+      signTransaction,
+    };
+
+    //Create Environment
+    const environment: Environment = constructEnvironment({
+      endpoint: process.env.JSON_RPC_ENDPOINT,
+      provider: _provider,
+      logger: cliLogger,
+      deployment: _deployment,
+      options: defaultOptions,
+      wallet: _wallet,
+      track: Tracks.TESTING,
+    });
+
+    //Logger Settings
+    const info = environment.logger(
+      'Midas-Technologies-AG/protocol:test-giveth',
+      LogLevels.INFO,
+    );
+    info('construct Environment was successfull');
+
+    //Create test Fund
+    const createTestFund = async (environment: Environment) => {
+      const fund = await setupFund(environment, 'test-giveth');
+      info('setup Fund was successfull');
+      return fund;
+    };
+    const testFund = createTestFund(environment);
+
+    //Using testFund
+    info('Hub address is:', testFund);
+
+    expect(5 == 5);
+    /*    expect(subtract(preFundMln, postFundMln).quantity).toEqual(
+      takerQuantity.quantity,
+    );*/
+  });
 });
-
-//Logger Settings
-const debug = environment.logger(
-  'Midas-Technologies-AG/protocol:test-giveth',
-  LogLevels.INFO,
-);
-debug('construct Environment was successfull');
-
-//Create test Fund
-const createTestFund = async (environment: Environment) => {
-  const fund = await setupFund(environment, 'test-giveth');
-  debug('setup Fund was successfull');
-  return fund;
-};
-const testFund = createTestFund(environment);
-
-//Using testFund
-debug('Hub address is:', testFund);
-
-export { testFund };
