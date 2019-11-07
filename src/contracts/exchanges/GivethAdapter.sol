2da94f08658a79442b394502f4ca1a08b1b17158
iemwill <willie@laubenheimer.eu>
2019-11-07 15:09:41 +0100

    add protocol functions

--- src/contracts/exchanges/GivethAdapter.sol
+++ src/contracts/exchanges/GivethAdapter.sol
@@ -2,15 +2,85 @@ pragma solidity ^0.4.25;
 pragma experimental ABIEncoderV2;
 
 import "GivethBridge.sol";
+import "ExchangeAdapter.sol";
+import "Vault.sol";
+import "Trading.sol";
+
 
 /*GivethAdapter enables  ERC20funds on @melonproject/protocol to donate giveth DAC's. (DecentralizedAltruisticCommun) */
 
 /**
  * The GivethAdapter contract does this and that...
  */
-contract GivethAdapter {
-    constructor() {
+contract GivethAdapter is ExchangeAdapter {
+
+    address public bridge;
+    uint64 public receiverDAC;
+
+    constructor(address _bridge, uint64 _receiverDAC) public {
+        bridge = _bridge;
+        receiverDAC = _receiverDAC;
     }
+
+    function makeOrder(
+        address _targetExchange,
+        address[6] _orderAddresses,
+        uint[8] _orderValues,
+        bytes32 _identifier,
+        bytes _makerAssetData,
+        bytes _takerAssetData,
+        bytes _signature
+    ) public onlyManager notShutDown {
+
+        Hub hub = getHub();
+
+        address makerAsset = _orderAddresses[2];
+        uint makerQuantity = _orderValues[0];
+        address bridge = _targetExchange;
+
+        ensureCanMakeOrder(makerAsset);
+
+        // Order parameter checks
+        getTrading().updateAndGetQuantityBeingTraded(makerAsset);
+        ensureNotInOpenMakeOrder(makerAsset);
+
+        // Get and approve makerAsset
+        approveMakerAsset(bridge, makerAsset,makerQuantity);
+        // Donate asset
+        GivethBridge(bridge).donateAndCreateGiver(msg.sender, )
+        
+        // Postprocess/Update
+        getAccounting().updateOwnedAssets(); 
+
+        //Maybe not needed.
+/*      getTrading().returnAssetToVault(makerAsset);
+*/      getTrading().orderUpdateHook(
+            bridge,
+            _identifier,
+            Trading.UpdateType.make,
+            [address(makerAsset), address(takerAsset)],
+            [makerQuantity, uint(0), uint(0)]
+        );
+    }
+
+    /// @notice needed to avoid stack too deep error
+    function approveMakerAsset(address _targetExchange, address _makerAsset, uint _makerQuantity)
+        internal
+    {
+        Hub hub = getHub();
+        Vault vault = Vault(hub.vault());
+        vault.withdraw(_makerAsset, _makerQuantity);
+        require(
+            ERC20(_makerAsset).approve(_targetExchange, _makerQuantity),
+            "Maker asset could not be approved"
+        );
+    }
+
+    function changeBridge (address _newBridge) onlyManager notShutDown returns(bool) public {
+        bridge = _newBridge;
+        return true;
+    }
+    
   
     function tester (
         address _bridge,
