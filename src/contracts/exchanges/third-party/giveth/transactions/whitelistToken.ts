bd9f840d6c1d80b5f67fa7f3a0056f1d03e347a2
iemwill <willie@laubenheimer.eu>
2019-11-18 13:42:54 +0100

    add whitelist token function

---
+++ src/contracts/exchanges/third-party/giveth/transactions/whitelistToken.ts
@@ -0,0 +1,25 @@
+import {
+  transactionFactory,
+  PrepareArgsFunction,
+} from '~/utils/solidity/transactionFactory';
+import { Contracts } from '~/Contracts';
+
+export interface whitelistTokenArgs {
+  tokenAddress: string;
+}
+
+const prepareArgs: PrepareArgsFunction<whitelistTokenArgs> = async (
+  _,
+  { tokenAddress },
+) => {
+  return [tokenAddress.toString(), true];
+};
+
+const whitelistToken = transactionFactory(
+  'whitelistToken',
+  Contracts.GivethBridge,
+  undefined,
+  prepareArgs,
+);
+
+export { whitelistToken };
