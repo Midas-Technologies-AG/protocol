f8191bac3e8dc1fc8cd5e6cfc8b2d9b1969a35a6
iemwill <willie@laubenheimer.eu>
2020-02-05 10:28:46 +0100

    Add deployMain.sh

---
+++ bin/deployMain.sh
@@ -0,0 +1,6 @@
+yarn deploy \
+  -T kyberPrice \
+  --config deployments/configs/test/main2.json \
+  --gas-price 8000000000 \
+  --keystore .keystore-midasAG.json \
+  --endpoint https://mainnet.infura.io/v3/e5a2adb18c614c76844f076b64ce5d27
